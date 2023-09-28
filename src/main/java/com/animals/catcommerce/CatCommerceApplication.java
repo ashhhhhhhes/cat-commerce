7ca432a55142ae0a3128f721b7fffbaeb55c992d
hong ash <ash.hong@milkcorp.kr>
2023-09-28 18:41:57 +0900

    Initial commit

---
+++ src/main/java/com/animals/catcommerce/CatCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.animals.catcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatCommerceApplication.class, args);
+    }
+
+}
